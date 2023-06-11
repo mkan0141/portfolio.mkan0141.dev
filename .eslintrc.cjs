72bd783accebe6c95dcceb202ebcb2fb929975b2
mkan0141 <kanzume0141@gmail.com>
2023-06-11 20:20:12 +0900

    main chore(prettier): add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 /** @type {import('eslint/lib/shared/types').ConfigData} */
 
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  extends: ['next/core-web-vitals', 'prettier'],
 };
---
+++ .prettierrc.cjs
@@ -0,0 +1,7 @@
+/** @type {import('prettier').Config} */
+
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  singleQuote: true,
+  printWidth: 90,
+};
